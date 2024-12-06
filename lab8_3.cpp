565803f38eabaa4ec2454e8cb805542a4c540b74
NKLRM <nekoloveramenxd@gmail.com>
2024-12-06 14:57:39 +0700

    lab8_3.cpp complete2

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -21,27 +21,28 @@ int main(){
     cout << "Enter your age: ";
     cin >> age;
     if(age<=25){
-        cout << "Enter your age: ";
+        cout << "Enter your height: ";
         cin >> height;
         if(height<100)
             cout << "Your character = Chopper";
         else if(height<180)
             cout << "Your character = Usopp";
         else{
-            cout << "Enter your age: ";
+            cout << "Enter your bounty: ";
             cin >> bounty;
-            if(bounty>11000000)
+            if(bounty>1100000000)
                 cout << "Your character = Zoro";
             else
                 cout << "Your character = Sanji";
         }
     }
     else if(age<=60){
+        cout << "Enter your bounty: ";
         cin >> bounty;
         if(bounty>500000000)
-            cout << "Your character = Franky";
-        else
             cout << "Your character = Jinbe";
+        else
+            cout << "Your character = Franky";
     }
     else
         cout << "Your character = Brook";
